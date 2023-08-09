faef21fd0b16f607fe2076224a05e6a74164f4f0
Hong Jiang <lazyseq@gmail.com>
2023-08-09 17:23:16 +0800

    initial commit

---
+++ init.el
@@ -0,0 +1,30 @@
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+      (bootstrap-version 6))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/radian-software/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
+
+(setq straight-use-package-by-default t)
+(straight-use-package 'use-package)
+(menu-bar-mode -1)
+(set-default 'indicate-empty-lines t)
+(setq display-line-numbers-type 'relative)
+
+(use-package exec-path-from-shell
+  :config
+  (when (memq window-system '(mac ns x))
+    (exec-path-from-shell-initialize))
+  (when (daemonp)
+  (exec-path-from-shell-initialize)))
+
+(use-package spacegray-theme
+  :config
+  (load-theme 'spacegray t)
+  (global-hl-line-mode 1))
