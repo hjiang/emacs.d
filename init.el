40db633ce3dfd71aa415c68ec6d3e3c28502873f
Hong Jiang <lazyseq@gmail.com>
2023-08-21 14:17:32 +0800

    add markdown-mode

--- init.el
+++ init.el
@@ -194,3 +194,10 @@
 (use-package yaml-mode)
 
 (use-package bazel)
+
+(use-package markdown-mode
+  :ensure t
+  :mode ("README\\.md\\'" . gfm-mode)
+  :init (setq markdown-command "pandoc")
+  :bind (:map markdown-mode-map
+              ("C-c C-e" . markdown-do)))
